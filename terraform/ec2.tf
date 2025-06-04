1a4171e26725d55b94b2ca9442a38d6aafdbbc9a
Luis Cavalcante <luis@coffeetech.com.br>
2025-06-04 20:19:38 -0300

    ec2
    Signed-off-by: Luis Cavalcante <luis@coffeetech.com.br>

---
+++ terraform/ec2.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners = [ "amazon" ]
+}
+
+resource "aws_launch_template" "ecs_lt" {
+  name_prefix   = "ecs-template"
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  key_name               = "personal-account"
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  iam_instance_profile {
+    name = "ecsInstanceRole"
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+
+  user_data = filebase64("${path.module}/ecs.sh")
+}
